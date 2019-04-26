af998105785fa3c13118dea6dadd0522c72de281
Eduardo Camillo <eduardo.ckf@gmail.com>
2019-04-26 12:21:30 -0300

    fix font to Fira Sans again

--- views/home.js
+++ views/home.js
@@ -41,18 +41,16 @@ const componentStyle = \`
             </appcode>
 
             <h3 class="title">Add CSS variables on helpers.js and use on SFC</h3>
-            <appcode>
-
+            <appcode :pre="true" filename="helpers.js">
     const maincolor = '#151515';
-    const subcolor = '#42b983';
-            </appcode>
+    const subcolor = '#42b983';</appcode>
             <appcode :pre="true" filename="component.js">
-    const componentStyle = \`
-        .classname {
-            font-size: 16px;
-            color: \$\{maincolor\}
-        }
-    \`</appcode>
+const componentStyle = \`
+    .classname {
+        font-size: 16px;
+        color: \$\{maincolor\}
+    }
+\`</appcode>
 
             <h3 class="title">Create routes, more components, more stuff</h3>
             <p>Go check the <a href="https://github.com/educkf/simple-vue-sfc" class="link" target="_blank">Github repo</a>, it is easier to understand there.</p>
